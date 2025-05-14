7755c13d645d7ce1bcda2595aea8e7dd0442e7a7
Rajat Gupta <rajatgupta87799@gmail.com>
2025-05-14 21:00:55 +0530

    Add files via upload

---
+++ ComparisionOperators.py
@@ -0,0 +1,16 @@
+a = float(input("Enter the number: "))
+b = float(input("Enter the number: "))
+
+Equalto = a == b
+GreaterThanEqualto = a >= b
+LessThanEqualto = a <= b
+GreaterThan = a > b
+LessThan = a < b
+NotEqualto = a != b
+
+print("Equal to: ", Equalto)
+print("Greater than equal to: ", GreaterThanEqualto)
+print("Less than equal to: ", LessThanEqualto)
+print("Greater than; ", GreaterThan)
+print("Less than: ", LessThan)
+print("Not equal to: ", NotEqualto)
