9b9fb1ee9b6b3fe42c1802003ce43e1bdea4e559
Rajat Gupta <rajatgupta87799@gmail.com>
2025-05-14 20:32:08 +0530

    Add files via upload

---
+++ EasyProject.py
@@ -0,0 +1,16 @@
+a = float(input("Enter the number: "))
+b = float(input("Enter the number: "))
+
+add = a + b 
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
+exp = a ** b
+
+print("Addition's result is: ", add)
+print("Substraction's result is: ", sub)
+print("Multiplication's result is: ", mul)
+print("Divition's result is: ", div)
+print("modulus's result is: ", mod)
+print("Exponent's result  is: ", exp)
